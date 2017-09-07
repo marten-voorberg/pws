ebd5192bf8bea633036dd423f31d3038da8c2029
MartenMoti <martenvoorberg@gmail.com>
2017-09-07 22:29:40 +0200

    Added animation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
 from body import Body
 from physics.vector2 import Vector2
+
+# Animation and Graphs
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # dt = 1e10
 #
@@ -21,9 +24,9 @@ b_y = []
 
 
 # dt = 24 * 60 * 60
-dt = 0.01
+dt = 0.1
 # simulated_time = dt * 100
-simulated_time = 8
+simulated_time = 30
 elapsed_time = 0
 bodies = [a, b]
 
@@ -60,9 +63,9 @@ while elapsed_time < simulated_time:
 
     elapsed_time += dt
 
-plt.plot(a_x, a_y, b_x, b_y)
-# plt.plot(a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y)
-plt.show()
+# plt.plot(a_x, a_y, b_x, b_y)
+# # plt.plot(a_x, a_y, b_x, b_y, c_x, c_y, d_x, d_y)
+# plt.show()
 
 # for i in range(500):
 #     for i in range(len(bodies)):
@@ -91,3 +94,29 @@ plt.show()
 #
 #     xs.append(earth.position.x)
 #     ys.append(earth.position.y)
+
+# Animation
+
+def _update_plot(i, fig, scat):
+    # scat.set_offsets(([0, i], [50, i], [100, i]))
+    scat.set_offsets(([a_x[i], a_y[i]], [b_x[i], b_y[i]]))
+    print('Frames: {}'.format(i))
+    return scat
+
+fig = plt.figure()
+
+x = [0, 50, 100]
+y = [0, 0, 0]
+
+ax = fig.add_subplot(111)
+ax.grid(True, linestyle = '-', color = '0.75')
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
+
+scat = plt.scatter(x, y, c = x)
+scat.set_alpha(0.8)
+
+anim = animation.FuncAnimation(fig, _update_plot, fargs = (fig, scat),
+                              frames = len(a_x), interval = 3)
+
+plt.show()
